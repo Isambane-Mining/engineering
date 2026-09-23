6beba010cfa28fd1c31f597221fb287b9d02eea5
Juan Frappe Lab Deployment <juan@isex2.isambane.co.za>
2026-09-23 09:48:52 +0200

    Add full-screen Service Schedule page

--- engineering/engineering/doctype/service_schedule/service_schedule.js
+++ engineering/engineering/doctype/service_schedule/service_schedule.js
@@ -17,7 +17,9 @@ function get_dashboard_wrapper(frm) {
 
 frappe.ui.form.on("Service Schedule", {
     refresh(frm) {
-
+        if (!frm.is_new()) {
+            frm.add_custom_button(__("Open Service Schedule"), () => frappe.set_route("service-schedule"));
+        }
 
 // ✅ Force OEM Booking checkbox to reflect doc value in grid list-view
 setTimeout(() => {
